5ff3502ccbc155d378de89a7ceb4e755cf09bf6e
Zefatan <Zefatan04@gmail.com>
2024-10-06 21:51:12 +0700

    feat(Zefa_Registration_User): add assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('account/register', [AccountController::class, 'register'])->name('account.register');
