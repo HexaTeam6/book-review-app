0bbe57f9c4694e2044b349956bd5e3d04ac03297
Abdur Rachman Wahed <abdurrachmanwahed@gmail.com>
2024-10-10 22:07:02 +0700

    feat(wahed/user-registration): add form validation

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -37,4 +37,15 @@ class AccountController extends Controller
     public function login() {
         return view('account.login');
     }
+
+    public function authenticate(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('account.login')->withInput()->withErrors($validator);
+        }
+    }
 }
