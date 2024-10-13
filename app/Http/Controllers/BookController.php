08364ac7fd41d3476c048f9551e2633a1f3eb85f
Abdur Rachman Wahed <abdurrachmanwahed@gmail.com>
2024-10-14 00:00:14 +0700

    feat(main/edit-book)

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Colors\Rgb\Channels\Red;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
 
@@ -66,5 +68,58 @@ class BookController extends Controller
         }
 
         return redirect()->route('books.index')->with('success', 'Book Added Successfully');
+    }
+
+    public function edit($id) {
+        $book = Book::findOrFail($id);
+        return view('books.edit', [
+            'book' => $book
+        ]);
+    }
+
+    public function update($id, Request $request) {
+        $book = Book::findOrFail($id);
+        $rules = [
+            'title' => 'required|min:5',
+            'author' => 'required|min:3',
+            'status' => 'required'
+        ];
+
+        if (!empty($request->image)) {
+            $rules['image'] = 'image';
+        }
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->route('books.edit', $book->id)->withInput()->withErrors($validator);
+        }
+
+        $book->title = $request->title;
+        $book->description = $request->description;
+        $book->author = $request->author;
+        $book->status = $request->status;
+        $book->save();
+
+        if (!empty($request->image)) {
+            File::delete(public_path('uploads/books/'.$book->image));
+            File::delete(public_path('uploads/books/thumb/'.$book->image));
+
+            $image = $request->image;
+            $extension = $image->getClientOriginalExtension();
+            $imageName = time().'.'.$extension;
+            $image->move(public_path('uploads/books'), $imageName);
+
+            $book->image = $imageName;
+            $book->save();
+
+            $manager = new ImageManager(Driver::class);
+            $uploadedImage = $manager->read(public_path('uploads/books/'.$imageName));
+
+            $uploadedImage->scale(700, 1000);
+            $uploadedImage->save(public_path('uploads/books/thumb/'.$imageName));
+        }
+
+        return redirect()->route('books.index')->with('success', 'Book Updated Successfully');
     }
 }
