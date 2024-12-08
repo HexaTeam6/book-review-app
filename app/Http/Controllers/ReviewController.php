4e43a606ff587112e6fa9a273940fa069456a683
Abdur Rachman Wahed <abdurrachmanwahed@gmail.com>
2024-12-08 19:04:26 +0700

    feat(main/edit-review-admin): add edit review admin only

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ReviewController extends Controller
 {
@@ -17,5 +18,29 @@ class ReviewController extends Controller
         return view('account.reviews.list', [
             'reviews' => $reviews
         ]);
+    }
+
+    public function edit($id) {
+        $review = Review::findOrFail($id);
+        return view('account.reviews.edit', [
+            'review' => $review
+        ]);
+    }
+
+    public function updateReview($id, Request $request) {
+        $review = Review::findOrFail($id);
+        $validator = Validator::make($request->all(), [
+            'status' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return redirect()->route('account.reviews.edit', $id)->withInput()->withErrors();
+        }
+
+        $review->status = $request->status;
+        $review->save();
+
+        session()->flash('success', 'Review update successfully.');
+        return redirect()->route('account.reviews');
     }
 }
